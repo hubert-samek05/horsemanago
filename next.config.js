72df7262683978b0a7595c65d3c03d3a33a6d2e9
root <root@debian13.ispot.cc>
2026-08-06 11:15:20 +0200

    iOS: fix launch crash, update public/index.html redirect, bump build to 6

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
     ];
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://horsemanago.net',
   },
 }
 
